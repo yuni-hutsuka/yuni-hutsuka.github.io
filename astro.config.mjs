4c9fa3cf5f0d10ed6bfaa6d0bfc399b04928bf48
yuni-hutsuka <yuni.wille999@gmail.com>
2023-04-07 18:21:54 +0900

    chore: edit astro.config.mjs
    デプロイ先をgithub pagesに変更した

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://yuni-hutsuka.github.io',
 	integrations: [mdx(), sitemap()],
 });
