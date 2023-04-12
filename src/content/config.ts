2e1b3949c55839ec81db473369ccddd1b2f0f61f
yuni-hutsuka <yuni.wille999@gmail.com>
2023-04-12 23:27:01 +0900

    feat: add const logs

--- src/content/config.ts
+++ src/content/config.ts
@@ -36,5 +36,22 @@ const article = defineCollection({
 	}),
 });
 
+const logs = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		heroImage: z.string().optional(),
+	}),
+});
 
-export const collections = { blog, article };
+export const collections = { blog, article, logs };
